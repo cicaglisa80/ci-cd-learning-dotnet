80f64a4efff82e2b1d8f253ff84039605fa8a9ac
Milos Kirjakovic <>
2026-03-24 14:55:00 +0000

    - added health check

--- Api/Program.cs
+++ Api/Program.cs
@@ -21,6 +21,8 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
+
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
